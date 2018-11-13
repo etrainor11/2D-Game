f343794a9c2539c9501807f6d28d1dd865eb85d7
etrainor11 <etrainor@hotmail.co.uk>
2018-11-13 15:41:28 +0000

    Added simple unity project

---
+++ 534 Game/Assets/Scripts/GitHubAcknowledge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubAcknowledge : MonoBehaviour {
+
+    private string[] names = { "eoin", "ryan", "steven", "lauren", "coramc" };
+
+	// Use this for initialization
+	void Start () {
+
+       
+        Debug.Log("This is " + names[0] +" standing by");
+
+        //Ryan, uncomment this one when on 
+        //Debug.Log("This is " + names[1] + " standing by");
+
+        //Steven, uncomment this one when on
+        //Debug.Log("This is " + names[2] + " standing by");
+
+        //Lauren, uncomment this one when on
+        //Debug.Log("This is " + names[3] + " standing by");
+
+        //Cormac, uncomment this one when on
+        //Debug.Log("This is " + names[4] + " standing by");
+    }
+	
+	
+}
