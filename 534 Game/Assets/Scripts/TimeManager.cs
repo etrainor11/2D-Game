56a6855bab7f4f422dc9f917effc639e1ce6e4eb
Pendleton117 <r_pendleton_1996@hotmail.com>
2019-01-03 16:14:41 +0000

    Added Background Animation and Countdown Timer to scene

---
+++ 534 Game/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    public float startingTime;
+
+    private Text theText;
+
+
+	// Use this for initialization
+	void Start () {
+
+        theText = GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        startingTime -= Time.deltaTime;
+
+        theText.text = "" + Mathf.Round(startingTime);
+
+	}
+}
